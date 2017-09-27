21214ec73a1884230f26940982d8ec19700fc03d
Peder Waern <pederwaern@hotmail.com>
2017-09-27 11:31:09 +0200

    support Custom delimiter

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,16 +1,46 @@
+import java.util.Arrays;
+
 public class StringCalculator {
+
+		private static int CUSTOM_DELIMETER_INDEX = 2;
+		private static String CUSTOM_DELIMITER_PATTERNSTART = "//";
+		private static String DEFAULT_DELIMITER = ",";
+		private static int CUSTOM_DELIMITER_PATTERNEND_INDEX = 4;
+		private static final int MIN_CUSTOM_DELIMITER_INPUT_LENGTH = 5;
 
 		public int add(String numbers) {
 				if (numbers.isEmpty()) {
 						return 0;
 				}
-				numbers = numbers.replaceAll("\n", ",");
-				String[] splitNumbers = numbers.split(",");
-				int result = 0;
-				for (String splitNumber : splitNumbers) {
-						result += Integer.parseInt(splitNumber);
+
+				final String delimiter;
+				final boolean hasValidCustomDelimiterPatternStart =
+								numbers.length() > MIN_CUSTOM_DELIMITER_INPUT_LENGTH
+												&& removeCustomDelimiterPatternStart(numbers, 0, 2)
+														   .equals(CUSTOM_DELIMITER_PATTERNSTART);
+				if (hasValidCustomDelimiterPatternStart) {
+						delimiter = parseCustomDelimiter(numbers);
+						numbers = removeCustomDelimiterPatternStart(numbers, CUSTOM_DELIMITER_PATTERNEND_INDEX, numbers.length());
+				} else {
+						delimiter = DEFAULT_DELIMITER;
 				}
-				return result;
+
+				String numbersWithNewLineReplaced = numbers.replaceAll("\n", delimiter);
+				return Arrays.stream(numbersWithNewLineReplaced.split(delimiter))
+								.mapToInt(Integer::parseInt)
+                .boxed()
+                .filter(number -> number >= 0)
+								.mapToInt(num -> num)
+								.sum();
+		}
+
+
+		private String removeCustomDelimiterPatternStart(String numbers, int customDelimiterPatternEndIndex, int length) {
+				return numbers.substring(customDelimiterPatternEndIndex, length);
+		}
+
+		private String parseCustomDelimiter(String numbers) {
+				return String.valueOf(numbers.charAt(CUSTOM_DELIMETER_INDEX));
 		}
 }
 
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -43,4 +43,10 @@ public class StringCalculatorTest {
 		public void newLineSeparatorGetTreatedAsCommaSeparator() throws Exception {
 				assertThat(actual.add("1\n2,3"), is(6));
 		}
+
+		@Test
+		public void supportCustomDelimeter() throws Exception {
+				assertThat(actual.add("//;\n1;2"), is(3));
+				assertThat(actual.add("//!\n1!2!3"), is(6));
+		}
 }
