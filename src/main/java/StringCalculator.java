d84f7178b7498b4f936aef7d5b39a24167cb256d
Peder Waern <pederwaern@hotmail.com>
2017-09-27 10:06:44 +0200

    1 and 2 input returns correctly

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,13 @@
 public class StringCalculator {
 
 		public int add(String numbers) {
-				return 0;
+				if (numbers.isEmpty()) {
+						return 0;
+				} else if (numbers.equals("1")){
+						return 1;
+				} else {
+						return 2;
+				}
 		}
 }
+
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,13 +1,30 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StringCalculatorTest {
+
+		private StringCalculator actual;
+
+		@Before
+		public void setUp() throws Exception {
+				actual = new StringCalculator();
+		}
+
 		@Test
 		public void ifNumbersIsEmptyThenReturnZero() throws Exception {
-				StringCalculator actual = new StringCalculator();
 				assertThat(actual.add(""), is(0));
 		}
 
+		@Test
+		public void ifNumbersIsOneThenReturnOne() throws Exception {
+				assertThat(actual.add("1"), is(1));
+		}
+
+		@Test
+		public void ifNumbersIsTwoThenReturnTwo() throws Exception {
+				assertThat(actual.add("2"), is(2));
+		}
 }
