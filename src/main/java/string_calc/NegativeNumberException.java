fdeeb96247d1a30a7d84f17a0b3c126ab0fd4ed2
Peder Waern <pederwaern@hotmail.com>
2017-09-27 12:35:19 +0200

    reformat indentation IntelliJ

--- src/main/java/string_calc/NegativeNumberException.java
+++ src/main/java/string_calc/NegativeNumberException.java
@@ -1,7 +1,7 @@
 package string_calc;
 
-public class NegativeNumberException extends RuntimeException{
-		public NegativeNumberException(String message) {
-				super(message);
-		}
+public class NegativeNumberException extends RuntimeException {
+	public NegativeNumberException(String message) {
+		super(message);
+	}
 }
--- src/test/java/string_calc/StringCalculatorTest.java
+++ src/test/java/string_calc/StringCalculatorTest.java
@@ -8,57 +8,57 @@ import static org.junit.Assert.assertThat;
 
 public class StringCalculatorTest {
 
-		private StringCalculator actual;
+	private StringCalculator actual;
 
-		@Before
-		public void setUp() throws Exception {
-				actual = new StringCalculator();
-		}
+	@Before
+	public void setUp() throws Exception {
+		actual = new StringCalculator();
+	}
 
-		@Test
-		public void ifNumbersIsEmptyThenReturnZero() throws Exception {
-				assertThat(actual.add(""), is(0));
-		}
+	@Test
+	public void ifNumbersIsEmptyThenReturnZero() throws Exception {
+		assertThat(actual.add(""), is(0));
+	}
 
-		@Test
-		public void ifNumbersIsOneThenReturnOne() throws Exception {
-				assertThat(actual.add("1"), is(1));
-		}
+	@Test
+	public void ifNumbersIsOneThenReturnOne() throws Exception {
+		assertThat(actual.add("1"), is(1));
+	}
 
-		@Test
-		public void ifNumbersIsTwoThenReturnTwo() throws Exception {
-				assertThat(actual.add("2"), is(2));
-		}
+	@Test
+	public void ifNumbersIsTwoThenReturnTwo() throws Exception {
+		assertThat(actual.add("2"), is(2));
+	}
 
-		@Test
-		public void canHandleSeveralNumbersSeparatedByComma() throws Exception {
-				assertThat(actual.add("1,2"), is(3));
-				assertThat(actual.add("1,2,2,3"), is(8));
-		}
+	@Test
+	public void canHandleSeveralNumbersSeparatedByComma() throws Exception {
+		assertThat(actual.add("1,2"), is(3));
+		assertThat(actual.add("1,2,2,3"), is(8));
+	}
 
-		@Test (expected = IllegalArgumentException.class)
-		public void nonNumberCharachterThrowsException() throws Exception {
-				actual.add("1,2,X");
-		}
+	@Test(expected = IllegalArgumentException.class)
+	public void nonNumberCharachterThrowsException() throws Exception {
+		actual.add("1,2,X");
+	}
 
-		@Test
-		public void newLineSeparatorGetTreatedAsCommaSeparator() throws Exception {
-				assertThat(actual.add("1\n2,3"), is(6));
-		}
+	@Test
+	public void newLineSeparatorGetTreatedAsCommaSeparator() throws Exception {
+		assertThat(actual.add("1\n2,3"), is(6));
+	}
 
-		@Test
-		public void supportCustomDelimeter() throws Exception {
-				assertThat(actual.add("//;\n1;2"), is(3));
-				assertThat(actual.add("//!\n1!2!3"), is(6));
-		}
+	@Test
+	public void supportCustomDelimeter() throws Exception {
+		assertThat(actual.add("//;\n1;2"), is(3));
+		assertThat(actual.add("//!\n1!2!3"), is(6));
+	}
 
-		@Test (expected = NegativeNumberException.class)
-		public void oneNegativeNumberThrowsException() throws Exception {
-				actual.add("1,2,-2,3");
-		}
+	@Test(expected = NegativeNumberException.class)
+	public void oneNegativeNumberThrowsException() throws Exception {
+		actual.add("1,2,-2,3");
+	}
 
-		@Test (expected = NegativeNumberException.class)
-		public void severalNegativeNumbersThrowsException() throws Exception {
-				actual.add("1,-2,-2,3,5,-34");
-		}
+	@Test(expected = NegativeNumberException.class)
+	public void severalNegativeNumbersThrowsException() throws Exception {
+		actual.add("1,-2,-2,3,5,-34");
+	}
 }
